b7d4454fc0535b0d65cf257b320d5713ce7cb494
Vinicius Stentzler <vin.stentzler@gmail.com>
2022-12-16 23:18:29 -0300

    hotFIX: cors policy preventing request from browser

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const colors = require('colors');
 const dotenv = require('dotenv').config();
 const {connectDB} = require('./config/db');
+const cors = require('cors');
 
 //Connect to MongoDB
 connectDB();
@@ -10,6 +11,8 @@ connectDB();
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+
+app.use(cors());
 
 app.get('/', (req, res) =>
 	res.status(200).send({message: 'Welcome to the Ticket-app'})
