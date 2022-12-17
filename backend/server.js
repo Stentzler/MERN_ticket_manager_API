174722cf36f4e8ab357fc6910d0355d68ec584af
Vinicius Stentzler <vin.stentzler@gmail.com>
2022-12-16 22:47:31 -0300

    fix for deployment

--- backend/server.js
+++ backend/server.js
@@ -3,20 +3,17 @@ const express = require('express');
 const colors = require('colors');
 const dotenv = require('dotenv').config();
 const {connectDB} = require('./config/db');
-const cors = require('cors');
 
 //Connect to MongoDB
 connectDB();
 
 const PORT = process.env.PORT || 5000;
 
+const app = express();
+
 app.get('/', (req, res) =>
 	res.status(200).send({message: 'Welcome to the Ticket-app'})
 );
-
-const app = express();
-
-app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
